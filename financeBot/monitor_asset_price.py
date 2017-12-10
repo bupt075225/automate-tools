21d7d6f5c12e91088ca8773c695574b0ae9194e4
Bill Liu <nonprivatemail@163.com>
2017-12-10 20:12:05 +0800

    add finance monitor feature

---
+++ financeBot/monitor_asset_price.py
@@ -0,0 +1,104 @@
+# -*- coding:utf-8 -*-
+
+from __future__ import division
+import time
+import sys
+sys.path.append("/home/data/git/data_source")
+from data_source import CoindeskDataSource
+from data_source import IEXDataSource
+from send_sms import SmsSender
+from private import configs
+
+class AssetPriceMonitor(object):
+    '''
+    监视资产价格,上涨或下跌超过阀值就发送短信
+
+    参数:
+    name: 资产名称,通常是股票代码
+    base: 初始价格，作为价格比较的初始基准
+    threshold: 价格上涨或下跌阀值,超过阀值就发送短信
+    '''
+    def __init__(self, name, base, threshold):
+        self.asset_name = name
+        self.base_price = base
+        self.threshold = threshold
+
+    def _get_cur_btc_price(self):
+        data_source = CoindeskDataSource()
+        try:
+            cur_price = data_source.get_realtime_btc()
+        except:
+            print "Get digital currency price failed"
+            return 0
+        return cur_price
+
+    def _get_cur_stock_price(self):
+        data_source = IEXDataSource(self.asset_name)
+        try:
+            rsp = data_source.get_realtime_quote()
+        except:
+            print "Get stock price failed"
+            return 0
+        return rsp["latestPrice"]
+
+    def get_quotes(self):
+        '''
+        计算当前资产价格上涨或下迭幅度
+        '''
+        quotes = {}
+        if self.asset_name == "BTC":
+            cur_price = self._get_cur_btc_price()
+        else:
+            cur_price = self._get_cur_stock_price()
+
+        if 0 == cur_price:
+            return quotes
+
+        delta = abs(cur_price - self.base_price)
+        percent = round(delta / self.base_price * 100, 2)
+        quotes["asset_name"] = self.asset_name
+        quotes["cur_price"] = cur_price
+        quotes["threshold"] = self.threshold
+        print percent
+        if cur_price < self.base_price and percent >= self.threshold:
+            quotes["state"] = "下跌"
+        elif cur_price > self.base_price and percent >= self.threshold:
+            quotes["state"] = "上涨"
+        else:
+            quotes = {}
+
+        return quotes
+
+def check_asset_quotes(name, base, threshold):
+    ret = {}
+    ret["name"] = name
+    ret["base"] = base
+    ret["threshold"] = threshold
+    monitor = AssetPriceMonitor(name, base, threshold)
+    quote = monitor.get_quotes()
+    if "state" in quote:
+        '''
+        资产状态超过阀值,更新价格比较基准
+        '''
+        ret["base"] = quote["cur_price"]
+        print "%s asset state change" % name
+        print quote
+        # 发送告警短信
+        params = []
+        # 追加元素到list中的顺序不能变,与短信模板中的参数顺序保持一致
+        params.append(quote["asset_name"])
+        params.append(quote["cur_price"])
+        params.append(quote["state"])
+        params.append(str(quote["threshold"]) + "%")
+        # 30秒内发送短信不能超过1条
+        time.sleep(35)
+        sender = SmsSender(configs["sms"]["appid"], configs["sms"]["appkey"])
+        sender.send_with_param(configs["sms"]["phone_numbers"], 
+                               configs["sms"]["templ_id"], 
+                               params, 
+                               configs["sms"]["sms_sign"])
+    else:
+        print "Check %s quotes nothing" % name
+
+    return ret
+
---
+++ financeBot/send_sms.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+
+'''
+使用腾讯云提供的短信服务发送短信通知
+'''
+
+from utils import SmsSenderUtil
+
+class SmsSender(object):
+    '''
+    群发短信
+
+    使用示例:
+    from private import configs
+
+    phone_numbers = configs["sms"]["phone_numbers"]
+    templ_id = configs["sms"]["templ_id"]
+    sign = configs["sms"]["sms_sign"]
+    params = ["BCH", "1500", "上涨", "10%"]
+
+    sender = SmsSender(configs["sms"]["appid"], configs["sms"]["appkey"])
+    ret = sender.send_with_param(phone_numbers, templ_id, params, sign)
+    '''
+
+    def __init__(self, appid=0, appkey=""):
+        self.url = "https://yun.tim.qq.com/v5/tlssmssvr/sendmultisms2"
+        self.appid = appid
+        self.appkey = appkey
+        self.util = SmsSenderUtil()
+
+    def send_with_param(self, phone_numbers, templ_id, params, sign, extend="", ext=""):
+        '''
+        按指定模板群发短信
+
+        Args:
+            phone_numbers: 不带国家码的手机号列表
+            templ_id: 模板 id
+            params: 模板参数列表，如模板 {1}...{2}...{3}，那么需要带三个参数
+            sign: 短信内容配置时申请的短信签名
+            extend: 扩展码，可填空串
+            ext: 服务端原样返回的参数，可填空串
+        '''
+
+        rnd = self.util.get_random()
+        cur_time = self.util.get_cur_time()
+        data = {}
+        data["tel"] = self.util.phone_numbers_to_list("86", phone_numbers)
+        #sign字段是短信标签，如果填空串，系统会使用默认:腾讯云
+        data["sign"] = sign
+        data["sig"] = self.util.sign(self.appkey, rnd, cur_time, phone_numbers)
+        data["tpl_id"] = templ_id
+        data["params"] = params
+        data["time"] = cur_time
+        data["extend"] = extend
+        data["ext"] = ext
+
+        whole_url = self.url + "?sdkappid=" + str(self.appid) + "&random=" + str(rnd)
+        return self.util.send_post_request("yun.tim.qq.com", whole_url, data)
+
